81f9a8a7ba008ab9605f1e0b527c1db185f27596
Vicente Merino <47841749+VicenteMerino@users.noreply.github.com>
2021-06-15 22:26:02 -0400

    Create GET exchange-rates endpoint (#2)

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   root 'pages#index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace 'api' do
+    namespace 'v1' do
+      get 'exchange-rates', :to => "exchange#exchange_rates"
+    end
+  end
 end
